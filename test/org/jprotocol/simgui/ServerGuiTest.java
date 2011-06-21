357b46b6334cc4fd327ccb5085648921bd95be99
Anders Eliasson <eliassonaand@yahoo.com>
2011-06-21 16:47:50 +0200

    refactorings

--- test/org/jprotocol/simgui/ServerGuiTest.java
+++ test/org/jprotocol/simgui/ServerGuiTest.java
@@ -1,17 +1,11 @@
 package org.jprotocol.simgui;
 
-import org.jprotocol.example.protocols.protocolstack.ServerHandlerHierarchy;
-import org.jprotocol.framework.handler.IFlushable;
-import org.jprotocol.framework.handler.IProtocolSniffer;
-import org.jprotocol.framework.handler.ProtocolState;
-import org.jprotocol.framework.logger.IProtocolLogger;
-import org.jprotocol.simgui.server.ServerGui;
 import org.junit.Test;
 
 public class ServerGuiTest {
 	@Test 
 	public void test() {
-		ServerGui sg = new ServerGui(new ServerHandlerHierarchy(new IFlushable.NullFlushable(), new ProtocolState(), new IProtocolSniffer.NullProtocolSniffer(), new IProtocolLogger.NullProtocolLogger()));
+//		ServerGui sg = new ServerGui(new ServerHandlerHierarchy(new IFlushable.NullFlushable(), new ProtocolState(), new IProtocolSniffer.NullProtocolSniffer(), new IProtocolLogger.NullProtocolLogger()));
 		
 	}
 }
